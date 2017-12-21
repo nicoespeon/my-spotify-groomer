226a787f6c8a96819ff95ba0bc0c39040714be93
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2017-12-21 22:26:10 +0100

    Rework the UI and the workflow
    Fetch user top tracks to determine tracks that are not listened much.
    Popularity was a metric calculated over all Spotify users => irrelevant.

--- web/elm/src/MySpotifyGroomer.elm
+++ web/elm/src/MySpotifyGroomer.elm
@@ -1,11 +1,11 @@
 port module MySpotifyGroomer exposing (..)
 
-import Html exposing (Html, program, button, div, span, text, a, img, h2)
-import Html.Attributes exposing (src, class)
-import Html.Events exposing (onClick)
+import Html exposing (Html, program, button, div, span, text, a, img, h1, h2, p, nav, form, label, select, option, i)
+import Html.Attributes exposing (src, class, for, id, value, name, disabled, selected, attribute)
+import Html.Events exposing (onClick, onInput)
 import Http exposing (Error, Request, header, emptyBody, jsonBody, expectJson)
 import Navigation exposing (load)
-import Json.Decode as Decode exposing (Decoder, at, list, string, int)
+import Json.Decode as Decode exposing (Decoder, at, list, string, int, bool)
 import Json.Encode as Encode exposing (object)
 import Task exposing (Task)
 
@@ -14,8 +14,8 @@ main : Program Never Model Msg
 main =
     program
         { init = init
-        , view = view
         , update = update
+        , view = view
         , subscriptions = subscriptions
         }
 
@@ -25,11 +25,16 @@ main =
 
 
 type alias Model =
-    { accessToken : String
-    , fetchStatus : FetchStatus
+    { accessToken : AccessToken
+    , state : State
     , user : User
     , playlists : List Playlist
+    , favoriteTracks : List TrackUri
     }
+
+
+type alias AccessToken =
+    String
 
 
 type alias User =
@@ -45,6 +50,7 @@ type alias UserId =
 
 type alias Playlist =
     { id : PlaylistId
+    , ownerId : UserId
     , name : String
     , tracksUrl : String
     , tracks : List Track
@@ -58,7 +64,7 @@ type alias PlaylistId =
 type alias Track =
     { uri : TrackUri
     , name : String
-    , popularity : Int
+    , isLocal : Bool
     }
 
 
@@ -66,23 +72,21 @@ type alias TrackUri =
     String
 
 
-type alias TracksResponse =
+type alias PaginatedTracks =
     { tracks : List Track
     , next : Maybe String
     }
 
 
-type FetchStatus
-    = NotStarted
-    | FetchingUser
-    | FetchingPlaylists
+type State
+    = Blank
+    | Loading
+    | Errored String
+    | LoadingPlaylists String
+    | PlaylistSelection
+    | LoadingTracks
+    | PlaylistTracks Playlist
     | DeletingTrack
-    | Error
-    | Success
-
-
-
--- INIT
 
 
 init : ( Model, Cmd Msg )
@@ -93,9 +97,10 @@ init =
 emptyModel : Model
 emptyModel =
     { accessToken = ""
-    , fetchStatus = NotStarted
+    , state = Blank
     , user = User "" "" Nothing
     , playlists = []
+    , favoriteTracks = []
     }
 
 
@@ -106,41 +111,104 @@ emptyModel =
 type Msg
     = LogIn AccessToken
     | UserFetched (Result Error User)
+    | FavoriteTracksFetched (Result Error (List TrackUri))
     | PlaylistsFetched (Result Error (List Playlist))
+    | SelectPlaylist PlaylistId
+    | PlaylistTracksFetched (Result Error Playlist)
     | DeleteTrack PlaylistId TrackUri
     | TrackDeleted (Result Error (List Playlist))
-
-
-type alias AccessToken =
-    String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LogIn accessToken ->
-            ( { model | fetchStatus = FetchingUser, accessToken = accessToken }, fetchUser accessToken )
+            ( { model | state = Loading, accessToken = accessToken }
+            , fetchUser accessToken
+            )
 
         UserFetched (Ok user) ->
-            ( { model | fetchStatus = FetchingPlaylists, user = user }, fetchPlaylistsWithTracks model.accessToken )
+            ( { model
+                | state = LoadingPlaylists "Fetching your favorite tracks…"
+                , user = user
+              }
+            , fetchFavoriteTracks model.accessToken
+            )
 
         UserFetched (Err _) ->
-            ( { model | fetchStatus = Error }, load "/login" )
+            ( { model | state = Errored "Failed to fetch user data." }
+            , load "/login"
+            )
+
+        FavoriteTracksFetched (Ok trackUris) ->
+            ( { model
+                | state = LoadingPlaylists "Fetching your playlists…"
+                , favoriteTracks = trackUris
+              }
+            , fetchPlaylists model.accessToken
+            )
+
+        FavoriteTracksFetched (Err _) ->
+            ( { model | state = Errored "Failed to fetch user top tracks." }
+            , Cmd.none
+            )
 
         PlaylistsFetched (Ok playlists) ->
-            ( { model | fetchStatus = Success, playlists = playlists }, Cmd.none )
+            ( { model
+                | state = PlaylistSelection
+                , playlists = ownedByUser model.user.id playlists
+              }
+            , Cmd.none
+            )
 
         PlaylistsFetched (Err _) ->
-            ( { model | fetchStatus = Error }, Cmd.none )
+            ( { model | state = Errored "Failed to fetch user playlists." }
+            , Cmd.none
+            )
+
+        SelectPlaylist playlistId ->
+            case selectedPlaylist playlistId model.playlists of
+                Just playlist ->
+                    ( { model | state = LoadingTracks }
+                    , fetchPlaylistTracks model.accessToken playlist model.favoriteTracks
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        PlaylistTracksFetched (Ok playlist) ->
+            ( { model | state = PlaylistTracks playlist }, Cmd.none )
+
+        PlaylistTracksFetched (Err _) ->
+            ( { model | state = Errored "Failed to fetch playlist tracks." }
+            , Cmd.none
+            )
 
         DeleteTrack playlistId trackUri ->
-            ( { model | fetchStatus = DeletingTrack }, deleteTrackFromPlaylist model playlistId trackUri )
+            ( { model | state = DeletingTrack }
+            , deleteTrackFromPlaylist model playlistId trackUri
+            )
 
         TrackDeleted (Ok playlists) ->
-            ( { model | fetchStatus = Success, playlists = playlists }, Cmd.none )
+            ( { model | state = PlaylistSelection, playlists = playlists }
+            , Cmd.none
+            )
 
         TrackDeleted (Err _) ->
-            ( { model | fetchStatus = Error }, Cmd.none )
+            ( { model | state = Errored "Failed to delete track." }
+            , Cmd.none
+            )
+
+
+ownedByUser : UserId -> List Playlist -> List Playlist
+ownedByUser userId =
+    List.filter (\p -> p.ownerId == userId)
+
+
+selectedPlaylist : PlaylistId -> List Playlist -> Maybe Playlist
+selectedPlaylist playlistId =
+    List.filter (\p -> p.id == playlistId)
+        >> List.head
 
 
 getFromSpotify : AccessToken -> String -> Decoder a -> Request a
@@ -169,63 +237,62 @@ fetchUser accessToken =
         Http.send UserFetched (getFromSpotify accessToken url userDecoder)
 
 
-fetchPlaylistsWithTracks : AccessToken -> Cmd Msg
-fetchPlaylistsWithTracks accessToken =
-    fetchPlaylists accessToken
-        |> Task.andThen (fetchPlaylistsTracks accessToken)
-        |> Task.attempt PlaylistsFetched
+fetchFavoriteTracks : AccessToken -> Cmd Msg
+fetchFavoriteTracks accessToken =
+    let
+        url =
+            "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=100"
+    in
+        Http.send FavoriteTracksFetched (getFromSpotify accessToken url trackUrisDecoder)
 
 
-fetchPlaylists : AccessToken -> Task Error (List Playlist)
+fetchPlaylists : AccessToken -> Cmd Msg
 fetchPlaylists accessToken =
     let
         url =
-            "https://api.spotify.com/v1/me/playlists?fields=items(id,name,tracks.href)&limit=50"
+            "https://api.spotify.com/v1/me/playlists?fields=items(id,owner.id,name,tracks.href)&limit=50"
     in
-        getFromSpotify accessToken url playlistsDecoder
-            |> Http.toTask
+        Http.send PlaylistsFetched (getFromSpotify accessToken url playlistsDecoder)
 
 
-fetchPlaylistsTracks : AccessToken -> List Playlist -> Task Error (List Playlist)
-fetchPlaylistsTracks accessToken playlists =
-    playlists
-        |> List.map (fetchPlaylistTracks accessToken)
-        |> Task.sequence
-
-
-fetchPlaylistTracks : AccessToken -> Playlist -> Task Error Playlist
-fetchPlaylistTracks accessToken playlist =
+fetchPlaylistTracks : AccessToken -> Playlist -> List TrackUri -> Cmd Msg
+fetchPlaylistTracks accessToken playlist favoriteTracks =
     let
         url =
-            playlist.tracksUrl ++ "?fields=items(track.name,track.popularity,track.uri),next"
+            playlist.tracksUrl ++ "?fields=items(track.name,track.uri,is_local),next"
     in
-        fetchPlaylistTracksWithUrl accessToken playlist url
+        fetchPlaylistTracksWithUrl accessToken playlist favoriteTracks url
+            |> Task.attempt PlaylistTracksFetched
 
 
-fetchPlaylistTracksWithUrl : AccessToken -> Playlist -> String -> Task Error Playlist
-fetchPlaylistTracksWithUrl accessToken playlist url =
-    getFromSpotify accessToken url tracksDecoder
-        |> Http.toTask
-        |> Task.andThen
-            (\tracks ->
-                case tracks.next of
-                    Just nextUrl ->
-                        updatePlaylistWithTracks playlist tracks.tracks
-                            |> Task.andThen
-                                (\newPlaylist -> fetchPlaylistTracksWithUrl accessToken newPlaylist nextUrl)
+fetchPlaylistTracksWithUrl : AccessToken -> Playlist -> List TrackUri -> String -> Task Error Playlist
+fetchPlaylistTracksWithUrl accessToken playlist favoriteTracks url =
+    let
+        addFavoriteTracksToPlaylist =
+            addTracksToPlaylist playlist favoriteTracks
+    in
+        getFromSpotify accessToken url paginatedTracksDecoder
+            |> Http.toTask
+            |> Task.andThen
+                (\paginatedTracks ->
+                    case paginatedTracks.next of
+                        Just nextUrl ->
+                            addFavoriteTracksToPlaylist paginatedTracks.tracks
+                                |> Task.andThen
+                                    (\newPlaylist -> fetchPlaylistTracksWithUrl accessToken newPlaylist favoriteTracks nextUrl)
 
-                    Nothing ->
-                        updatePlaylistWithTracks playlist tracks.tracks
-            )
+                        Nothing ->
+                            addFavoriteTracksToPlaylist paginatedTracks.tracks
+                )
 
 
-updatePlaylistWithTracks : Playlist -> List Track -> Task Error Playlist
-updatePlaylistWithTracks playlist tracks =
+addTracksToPlaylist : Playlist -> List TrackUri -> List Track -> Task Error Playlist
+addTracksToPlaylist playlist favoriteTracks tracks =
     let
         newTracks =
             playlist.tracks
                 |> List.append tracks
-                |> List.filter (\track -> track.popularity < 20)
+                |> List.filter (\track -> not (List.member track.uri favoriteTracks))
     in
         Task.succeed { playlist | tracks = newTracks }
 
@@ -271,7 +338,10 @@ playlistsWithoutTrack trackUri =
     List.map
         (\playlist ->
             { playlist
-                | tracks = List.filter (\track -> track.uri /= trackUri) playlist.tracks
+                | tracks =
+                    List.filter
+                        (\track -> track.uri /= trackUri)
+                        playlist.tracks
             }
         )
 
@@ -295,8 +365,9 @@ playlistsDecoder : Decoder (List Playlist)
 playlistsDecoder =
     at [ "items" ]
         (Decode.list
-            (Decode.map4 Playlist
+            (Decode.map5 Playlist
                 (at [ "id" ] string)
+                (at [ "owner", "id" ] string)
                 (at [ "name" ] string)
                 (at [ "tracks", "href" ] string)
                 (Decode.succeed [])
@@ -304,18 +375,28 @@ playlistsDecoder =
         )
 
 
-tracksDecoder : Decoder TracksResponse
+trackUrisDecoder : Decoder (List TrackUri)
+trackUrisDecoder =
+    at [ "items" ]
+        (Decode.list (at [ "uri" ] string))
+
+
+tracksDecoder : Decoder (List Track)
 tracksDecoder =
-    Decode.map2 TracksResponse
-        (at [ "items" ]
-            (Decode.list
-                (Decode.map3 Track
-                    (at [ "track", "uri" ] string)
-                    (at [ "track", "name" ] string)
-                    (at [ "track", "popularity" ] int)
-                )
+    at [ "items" ]
+        (Decode.list
+            (Decode.map3 Track
+                (at [ "track", "uri" ] string)
+                (at [ "track", "name" ] string)
+                (at [ "is_local" ] bool)
             )
         )
+
+
+paginatedTracksDecoder : Decoder PaginatedTracks
+paginatedTracksDecoder =
+    Decode.map2 PaginatedTracks
+        tracksDecoder
         (Decode.maybe (at [ "next" ] string))
 
 
@@ -325,35 +406,98 @@ tracksDecoder =
 
 view : Model -> Html Msg
 view model =
-    case model.fetchStatus of
-        NotStarted ->
-            text "Fetch of data has not started yet."
+    case model.state of
+        Blank ->
+            text ""
 
-        FetchingUser ->
-            text "Fetching user data from Spotify…"
+        Loading ->
+            loader "Fetching data from Spotify…"
 
-        Error ->
-            text "Something went wrong when data were fetched!"
-
-        FetchingPlaylists ->
+        LoadingPlaylists message ->
             div []
-                [ renderUser model.user
-                , text "Fetching playlists data from Spotify…"
+                [ mainNav model.user
+                , mainContainer [ pageTitle, loaderBlock message ]
+                ]
+
+        PlaylistSelection ->
+            div []
+                [ mainNav model.user
+                , mainContainer [ pageTitle, playlistSelectForm model.playlists ]
+                ]
+
+        LoadingTracks ->
+            div []
+                [ mainNav model.user
+                , mainContainer
+                    [ pageTitle
+                    , playlistSelectForm model.playlists
+                    , loaderBlock "Fetching playlist tracks…"
+                    ]
+                ]
+
+        PlaylistTracks playlist ->
+            div []
+                [ mainNav model.user
+                , mainContainer
+                    [ pageTitle
+                    , playlistSelectForm model.playlists
+                    , renderPlaylistTracks playlist
+                    ]
                 ]
 
         DeletingTrack ->
             div []
-                [ renderUser model.user
-                , text "Deleting track…"
+                [ mainNav model.user
+                , mainContainer [ pageTitle, loaderBlock "Deleting the track…" ]
                 ]
 
-        Success ->
-            div []
-                [ renderUser model.user
-                , div
-                    [ class "ui relaxed divided list" ]
-                    (List.map renderPlaylist model.playlists)
-                ]
+        Errored message ->
+            text ("An error occurred: " ++ message)
+
+
+mainContainer : List (Html Msg) -> Html Msg
+mainContainer children =
+    div [ class "ui main text container" ] children
+
+
+pageTitle : Html Msg
+pageTitle =
+    h1 [] [ text "Groom your Spotify playlists" ]
+
+
+loader : String -> Html Msg
+loader message =
+    div [ class "ui active inverted dimmer" ]
+        [ div
+            [ class "ui text loader" ]
+            [ text message ]
+        ]
+
+
+loaderBlock : String -> Html Msg
+loaderBlock message =
+    div
+        [ class "ui segment" ]
+        [ loader message
+        , img
+            [ class "ui wireframe image"
+            , src "/images/wireframe/short-paragraph.png"
+            ]
+            []
+        ]
+
+
+mainNav : User -> Html Msg
+mainNav user =
+    nav
+        [ class "ui fixed borderless secondary pointing nav menu" ]
+        [ div
+            [ class "ui container" ]
+            [ div
+                [ class "header item" ]
+                [ renderUser user ]
+            ]
+        ]
 
 
 renderUser : User -> Html Msg
@@ -369,25 +513,82 @@ renderUser user =
             text ("Hello " ++ user.name)
 
 
-renderPlaylist : Playlist -> Html Msg
-renderPlaylist playlist =
-    div [ class "item" ]
-        [ h2 []
-            [ text (playlist.name ++ " (" ++ toString (List.length playlist.tracks) ++ ")")
+playlistSelectForm : List Playlist -> Html Msg
+playlistSelectForm playlists =
+    let
+        placeholderOption =
+            option [ disabled True, selected True ] [ text "🎧 Select a playlist" ]
+
+        playlistOptions =
+            placeholderOption
+                :: List.map
+                    (\p -> option [ value p.id ] [ text p.name ])
+                    playlists
+    in
+        form
+            [ class "ui form" ]
+            [ div
+                [ class "field" ]
+                [ label [] [ text "Which playlist would you like to groom?" ]
+                , select [ class "ui fluid dropdown", onInput SelectPlaylist ] playlistOptions
+                ]
             ]
-        , div [ class "ui list" ]
-            (playlist.tracks
-                |> List.sortBy .popularity
-                |> List.map (renderTrack playlist.id)
-            )
+
+
+renderPlaylistTracks : Playlist -> Html Msg
+renderPlaylistTracks playlist =
+    div []
+        [ h2
+            [ class "ui section horizontal divider header" ]
+            [ i [ class "music icon" ] [], text playlist.name ]
+        , p
+            []
+            [ text
+                ("Here are the "
+                    ++ (playlist.tracks |> List.length |> toString)
+                    ++ " songs you don't listen much in this playlist:"
+                )
+            ]
+        , div
+            [ class "ui divided relaxed list" ]
+            (List.map (renderTrack playlist.id) playlist.tracks)
         ]
 
 
 renderTrack : PlaylistId -> Track -> Html Msg
 renderTrack playlistId track =
     div [ class "item" ]
-        [ button [ onClick (DeleteTrack playlistId track.uri) ] [ text "x" ]
-        , text ("(" ++ (toString track.popularity) ++ ") " ++ track.name)
+        [ div
+            [ class "right floated content" ]
+            [ deleteTrackButton playlistId track ]
+        , div
+            [ class "content" ]
+            [ p [ class "header" ] [ text track.name ] ]
+        ]
+
+
+deleteTrackButton : PlaylistId -> Track -> Html Msg
+deleteTrackButton playlistId track =
+    if track.isLocal then
+        disabledDeleteButton "I can't delete local files from your playlist (yet)"
+    else
+        deleteButton (DeleteTrack playlistId track.uri)
+
+
+deleteButton : Msg -> Html Msg
+deleteButton onClickAction =
+    button
+        [ class "ui icon red button", onClick onClickAction ]
+        [ i [ class "trash outline icon" ] [] ]
+
+
+disabledDeleteButton : String -> Html Msg
+disabledDeleteButton reason =
+    div
+        [ attribute "data-tooltip" reason, attribute "data-inverted" "" ]
+        [ button
+            [ class "ui icon disabled red button" ]
+            [ i [ class "trash outline icon" ] [] ]
         ]
 
 
